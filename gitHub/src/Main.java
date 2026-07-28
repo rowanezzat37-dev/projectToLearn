68313636a8d7245b7930180b40aababb4ede8f0d
Rowan <rowanezzat37@gmail.com>
2026-07-28 14:06:15 -0700

    added more lines

--- gitHub/src/Main.java
+++ gitHub/src/Main.java
@@ -2,4 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     System.out.println("Hello gitHub !");
+    System.out.println("Hello gitHub !");
+    System.out.println("Hello gitHub !");
 }
